7db53d5e117441fb4ee2cca034c42ad493787c1e
Takafumi Arakaki <aka.tkf@gmail.com>
2019-09-25 16:41:13 -0700

    Setup CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
+using VegaLite
 using VegaStreams
 using Test
 
 @testset "VegaStreams.jl" begin
-    # Write your own tests here.
+    vls = vegastream(@vlplot(:line, x=:x, y=:y))
+
+    for (x, y) in enumerate(randn(100))
+        sleep(0.01)
+        push!(vls, (x=x, y=y))
+    end
 end
